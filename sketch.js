d724322a9517a306b04c23391c0c4db9e799b980
vidhi3030 <j30vidhi@gmail.com>
2020-10-29 17:46:56 +0530

    bye

--- sketch.js
+++ sketch.js
@@ -1,28 +1,15 @@
-var box1,box2;
-function setup() {
-  createCanvas(800,400);
- box1= createSprite(400, 400, 50, 50);
- box2= createSprite(50,50,50,50);
- box1.shapeColor="blue";
- box2.shapeColor="hotpink";
- box1.velocityX=-1;
- box1.velocityY=-1;
- box2.velocityX=1;
- box2.velocityY=1;
+var cake,cakeimage;
+function preload (){
+  cakeimage= loadImage("cake.jpg");
 }
+function setup() {
+  createCanvas(400,400);
+ cake= createSprite(200, 200, 150, 150);
+ cake.addImage(cakeimage);
+ }
 
 function draw() {
   background(255,255,255); 
-  if (detectcollision(box1,box2)){
-      box1.shapeColor="green";
-      box2.shapeColor="black";
-      
-    /*}else{
-      box1.shapeColor="blue";
-      box2.shapeColor="hotpink";
-*/
-    }
-bounceOff(box1,box2);
   drawSprites();
 
 }
